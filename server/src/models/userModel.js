7c915403de2ed3d7e8ecafdc3eebe5be2a8e659f
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-12-07 18:35:41 +0100

    creation squelette serveur projet

---
+++ server/src/models/userModel.js
@@ -0,0 +1,14 @@
+/*const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const dbName = 'your_database';
+
+// Connectez-vous à la base de données
+async function connectToDatabase() {
+}
+
+// Obtenez tous les utilisateurs depuis la base de données
+async function getAllUsers() {
+}
+
+module.exports = { connectToDatabase, getAllUsers };
+*/
---
+++ server/src/utils/database.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+
+// Fonction utilitaire pour se connecter à la base de données
+async function connectToDatabase() {
+  //...
+}
+
+module.exports = { connectToDatabase };
